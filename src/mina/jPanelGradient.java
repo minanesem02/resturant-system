60a5d795e2d9311387c078c91d1de2df7d832daf
minanesem02 <gmina8143@gmail.com>
2024-06-11 01:08:12 +0300

    Add files via upload

---
+++ src/mina/jPanelGradient.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Thomas;
+
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics2D;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author CM
+ */
+public class jPanelGradient extends JPanel {
+ public (java.awt.Graphics2D g) {
+   Graphics2D g2d = (Graphics2D) g;
+int width = getWidth();
+
+int height = getHeight();
+
+Color colorl = new Color (52,50,80);
+
+Color color2= new Color (86,180,211);
+GradientPaint gp = new GradientPaint (0,0,colorl, 180, height,color2);
+
+g2d.setPaint (gp);
+
+g2d.fillRect(0, 0, width, height);
+
+    }
+    
+}
